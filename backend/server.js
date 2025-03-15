15e3e5b439ccd106aa9657cb91910e49a01dbfab
td jayadeera <edu.tharusha@gmail.com>
2025-03-15 09:16:07 +0530

    /chore /backend :initial project setup

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// test route
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
