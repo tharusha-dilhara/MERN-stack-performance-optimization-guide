726fb77c8904440dada9d975eb9d76d7bf846d71
td jayadeera <edu.tharusha@gmail.com>
2025-03-15 13:13:03 +0530

    /enhancement /database  :add index on 'name' field to optimize query performance

--- backend/models/item.js
+++ backend/models/item.js
@@ -4,5 +4,5 @@ const itemSchema = new mongoose.Schema({
   name: String,
   description: String
 });
-
+itemSchema.index({ name: 1 });  // Create an index on the 'name' field
 module.exports = mongoose.model('Item', itemSchema);
